1519576b9699fdaf201932bf5b5701cd3fa6c72e
hlb7146 <hlb7146@rit.edu>
2016-04-20 14:53:13 -0400

    Added comments

--- Game1/Game1/CharacterAttributes.cs
+++ Game1/Game1/CharacterAttributes.cs
@@ -23,6 +23,7 @@ namespace Game1
         public decimal dexterity;
         public decimal previousDexterity;
 
+        // fields for reading data
         int count = 0;
         string h;
         string s;
@@ -48,6 +49,7 @@ namespace Game1
 
         }
 
+        // change value of pointsNumeric based on healthNumeric value
         private void healthNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
             health = healthNumericUpDown.Value;
@@ -62,6 +64,7 @@ namespace Game1
             previousHealth = health;
         }
 
+        // change value of pointsNumeric based on strengthNumeric value
         private void strengthNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
             strength = strengthNumericUpDown.Value;
@@ -76,6 +79,7 @@ namespace Game1
             previousStrength = strength;
         }
 
+        // change value of pointsNumeric based on dexterityNumeric value
         private void dexterityNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
             dexterity = dexterityNumericUpDown.Value;
@@ -92,6 +96,7 @@ namespace Game1
 
         private void submitButton_Click(object sender, EventArgs e)
         {
+            // write data to file and display message confirming the submission
             WriteData(file);
             label6.Visible = true;
         }
@@ -119,9 +124,9 @@ namespace Game1
             try
             {
                 output = new StreamWriter(file);
-                output.WriteLine(healthNumericUpDown.Value);
                 output.WriteLine(strengthNumericUpDown.Value);
                 output.WriteLine(dexterityNumericUpDown.Value);
+                output.WriteLine(healthNumericUpDown.Value);
             }
             catch (Exception e)
             {
@@ -134,6 +139,7 @@ namespace Game1
             }
         }
 
+        // read data from file
         public void ReadData(String file)
         {
             StreamReader input = null;
@@ -145,20 +151,22 @@ namespace Game1
                 String line = null;
                 while ((line = input.ReadLine()) != null)
                 {
+                    // count determines where the current line is stored
                     if(count == 0)
-                    {
-                        h = line;
-                    }
-                    if(count == 1)
                     {
                         s = line;
                     }
-                    if(count == 2)
+                    if(count == 1)
                     {
                         d = line;
                     }
+                    if(count == 2)
+                    {
+                        h = line;
+                    }
                     count++;
                 }
+                // reset count for next use
                 count = 0;
             }
             catch (Exception e)
@@ -174,6 +182,7 @@ namespace Game1
 
         private void pointsNumeric_ValueChanged(object sender, EventArgs e)
         {
+            // disable all numerics if pointsNumeric is 0
             if (pointsNumeric.Value == 0)
             {
                 healthNumericUpDown.Enabled = false;
@@ -186,14 +195,17 @@ namespace Game1
         {
             ReadData(file);
 
+            // convert loaded data into decimals so it can be used by the numerics
             decimal loadHealth = System.Convert.ToDecimal(h);
             decimal loadStrength = System.Convert.ToDecimal(s);
             decimal loadDexterity = System.Convert.ToDecimal(d);
 
+            // change numeric values
             healthNumericUpDown.Value = loadHealth;
             strengthNumericUpDown.Value = loadStrength;
             dexterityNumericUpDown.Value = loadDexterity;
 
+            // change initial values
             health = healthNumericUpDown.Value;
             previousHealth = health;
             strength = strengthNumericUpDown.Value;
@@ -201,6 +213,7 @@ namespace Game1
             dexterity = dexterityNumericUpDown.Value;
             previousDexterity = dexterity;
 
+            // ensure pointsNumeric is set to proper number of points left after loading data
             pointsNumeric.Value = 5 - health - strength - dexterity;
         }
     }
