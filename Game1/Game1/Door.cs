1136a91b65d5d4a0140b922ae1d4b03b6def4a85
sankethcb <sankethcb@gmail.com>
2016-04-20 20:20:44 -0400

    Completely changed how doors work, map is fully functioning. Only enemies left to fix

--- Game1/Game1/Door.cs
+++ Game1/Game1/Door.cs
@@ -11,7 +11,8 @@ namespace Game1
     {
         public Texture2D sprite;
         public Rectangle position;
-
+        public Rectangle exitBox;
+        public bool active=true;
         public Door()
         {
 
@@ -19,11 +20,30 @@ namespace Game1
         public Door(int w, int h,int x, int y)
         {
             position = new Rectangle(x, y, w, h);
+            exitBox = new Rectangle(0, 0, 0, 0);
                         
         }
         public void SetSprite(Texture2D texture)
         {
             sprite = texture;
+        }
+
+        public void LeftEB()
+        {
+            exitBox = new Rectangle(0, 400, 80, 150);
+        }
+
+        public void RightEB()
+        {
+            exitBox = new Rectangle(1600, 400, 80, 150);
+        }
+        public void TopEB()
+        {
+            exitBox = new Rectangle(750, 0, 150, 80);
+        }
+        public void BottomEB()
+        {
+            exitBox = new Rectangle(750, 900, 150, 80);
         }
 
     }
--- Game1/Game1/Wall.cs
+++ Game1/Game1/Wall.cs
@@ -9,15 +9,15 @@ namespace Game1
 {
     public class Wall
     {
-        public Door wallDoor; 
+
         public Rectangle roomWall;
-        public Rectangle exitBox;
+   
         public Texture2D texture;
         public bool active = true;
         //walls
         public Wall()
         {
-            wallDoor = new Door();
+
         }
         
         enum wallType { Full, Door, Open};
@@ -26,52 +26,30 @@ namespace Game1
         public void SetTopWall()
         {
             roomWall = new Rectangle(0, 0, 1600, 50);
-            if (active)
-            {
-                wallDoor = new Door(150, 50, 750, 0);
-                exitBox = new Rectangle(750,0,150,80);
-                
-            }
+
 
         }
 
         public void SetBottomWall()
         {
-            roomWall = new Rectangle(0, 850, 1600, 50);
-            if (active)
-            {
-                wallDoor = new Door(150, 50, 750, 850);
-                exitBox = new Rectangle(750, 900, 150, 80);
-                
-            }
-            
+            roomWall = new Rectangle(0, 850, 1600, 50);         
 
         }
 
         public void SetLeftWall()
         {
             roomWall = new Rectangle(0, 0, 50, 900);
-            if (active)
-            {
-                wallDoor = new Door(50, 150, 0, 400);
-                exitBox = new Rectangle(0, 400, 80, 150);
-                
-            }
+
         }
 
         public void SetRightWall()
         {
             roomWall = new Rectangle(1550, 0, 50, 900);
-            if (active)
-            {
-                wallDoor = new Door(50, 150, 1550, 400);
-                exitBox = new Rectangle(1600, 400, 80, 150);
-                
-            }
 
         }
 
-        
+
+
 
 
     }
